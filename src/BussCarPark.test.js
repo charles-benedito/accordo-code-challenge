0e38146d257743f1cc2567482eee901cf6905529
Charles <deadfish.charles@gmail.com>
2018-12-04 15:00:37 +1300

    chai assertions on  methods left and right

--- src/BussCarPark.test.js
+++ src/BussCarPark.test.js
@@ -116,6 +116,7 @@ describe('Buss Car Park Simulator', () => {
             expect(simulator.x).to.equal(2);
             expect(simulator.y).to.equal(3);
         });
+        
         it('should move one unit to south when facing south', () => {
             let x = 2,
                 y = 2,
@@ -125,6 +126,7 @@ describe('Buss Car Park Simulator', () => {
             expect(simulator.x).to.equal(2);
             expect(simulator.y).to.equal(1);
         });
+
         it('should move one unit to west when facing west', () => {
             let x = 2,
                 y = 2,
@@ -134,6 +136,7 @@ describe('Buss Car Park Simulator', () => {
             expect(simulator.x).to.equal(1);
             expect(simulator.y).to.equal(2);
         });
+
         it('should move one unit to east when facing east', () => {
             let x = 2,
                 y = 2,
@@ -145,6 +148,80 @@ describe('Buss Car Park Simulator', () => {
         });
     });
 
+    describe('Method right', () => {
+        it('should face east after turn 90° right when facing north', () => {
+            simulator.place(null, null, 'north');
+            simulator.right();
+            expect(simulator.facing).to.equal('east');
+        });
+
+        it('should face west after turn 90° right when facing south', () => {
+            simulator.place(null, null, 'south');
+            simulator.right();
+            expect(simulator.facing).to.equal('west');
+        });
+
+        it('should face sout after turn 90° right when facing east', () => {
+            simulator.place(null, null, 'east');
+            simulator.right();
+            expect(simulator.facing).to.equal('south');
+        });
+
+        it('should face north after turn 90° right when facing west', () => {
+            simulator.place(null, null, 'west');
+            simulator.right();
+            expect(simulator.facing).to.equal('north');
+        });
+    });
+
+    describe('Method left', () => {
+        it('should face west after turn 90° left when facing north', () => {
+            simulator.place(null, null, 'north');
+            simulator.left();
+            expect(simulator.facing).to.equal('west');
+        });
+
+        it('should face east after turn 90° left when facing south', () => {
+            simulator.place(null, null, 'south');
+            simulator.left();
+            expect(simulator.facing).to.equal('east');
+        });
+
+        it('should face north after turn 90° left when facing east', () => {
+            simulator.place(null, null, 'east');
+            simulator.left();
+            expect(simulator.facing).to.equal('north');
+        });
+
+        it('should face south after turn 90° left when facing west', () => {
+            simulator.place(null, null, 'west');
+            simulator.left();
+            expect(simulator.facing).to.equal('south');
+        });
+    });
+
+    describe('Integrations', () => {
+        it('should report position 0, 1, north', () => {
+            simulator.place(0, 0, 'north');
+            simulator.move();
+            expect(simulator.report()).to.deep.equal({x: 0, y: 1, facing: 'north'});
+        });
+
+        it('should report position 0, 0, west', () => {
+            simulator.place(0, 0, 'north');
+            simulator.left();
+            expect(simulator.report()).to.deep.equal({x: 0, y: 0, facing: 'west'});
+        });
+
+        it('should report position 3, 3, north', () => {
+            simulator.place(1, 2, 'east');
+            simulator.move();
+            simulator.move();
+            simulator.left();
+            simulator.move();
+            expect(simulator.report()).to.deep.equal({x: 3, y: 3, facing: 'north'});
+        });
+    });
     
 
 });
